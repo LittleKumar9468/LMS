ecd1a0d62a628c93914fff0f70a0b8c52a34b788
Little Kumar <littletiwary9468@gmail.com>
2026-06-01 02:18:39 +0530

    Fix gmail smtp config

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,14 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-
 import nodemailer from "nodemailer";
 
 console.log("EMAIL_USER =", process.env.EMAIL_USER);
 console.log("EMAIL_PASS =", process.env.EMAIL_PASS);
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
